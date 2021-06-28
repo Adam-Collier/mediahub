8dbbb1fd6b0ed7b3a8640f443e52572f126c1d41
Adam Collier <adamcollierart@gmail.com>
2021-06-28 15:59:17 +0100

    feature: use postcss so we can use modern syntax

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,25 @@ const browserSync = require( 'browser-sync' ).create();
 const sass = require( 'gulp-sass' );
 const rename = require( 'gulp-rename' );
 
+const postcss = require( 'gulp-postcss' );
+const presetEnv = require( 'postcss-preset-env' );
+
 const scss = () => {
+	const plugins = [
+		presetEnv( {
+			autoprefixer: {
+				flexbox: 'no-2009',
+			},
+			stage: 3,
+			features: {
+				'custom-properties': false,
+			},
+		} ),
+	];
+
 	return src( './sass/style.scss' )
 		.pipe( sass() )
+		.pipe( postcss( plugins ) )
 		.pipe( rename( 'style.css' ) )
 		.pipe( dest( './', { overwrite: true } ) );
 };
