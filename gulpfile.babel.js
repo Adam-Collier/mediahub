5eecd7f91686bd00e57224e08d2b09565a29d98f
Adam Collier <adamcollierart@gmail.com>
2021-07-08 09:18:56 +0100

    feature: use babel for gulp so we can use imports

---
+++ gulpfile.babel.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-undef */
+import { src, dest, series, watch } from 'gulp';
+import sass from 'gulp-sass';
+import rename from 'gulp-rename';
+import postcss from 'gulp-postcss';
+import presetEnv from 'postcss-preset-env';
+
+// added packages
+import browserSync from 'browser-sync';
+import util from 'util';
+import fs from 'fs-extra';
+import path from 'path';
+import child_process from 'child_process';
+import del from 'del';
+
+let exec = util.promisify(child_process.exec);
+
+const scss = () => {
+	const plugins = [
+		presetEnv( {
+			autoprefixer: {
+				flexbox: 'no-2009',
+			},
+			stage: 3,
+			features: {
+				'custom-properties': false,
+			},
+		} ),
+	];
+
+	return src( './sass/style.scss' )
+		.pipe( sass() )
+		.pipe( postcss( plugins ) )
+		.pipe( rename( 'style.css' ) )
+		.pipe( dest( './', { overwrite: true } ) );
+};
+
+export const compress = async () => {
+  let date = new Date().toISOString().replace('T', ' ').substr(0, 10);
+  let fileName = `mediahub_${date}`;
+
+  // copy the babezine folder outside babezine (can't copy inside)
+  await fs.copy(path.resolve(__dirname, './'), `../${fileName}`, {
+    overwrite: true,
+    filter: (path) => {
+      return ![
+        '/node_modules',
+        '.gitignore',
+        '.babelrc',
+        'gulpfile.babel.js',
+        'package.json',
+        'package-lock.json',
+        '/sass',
+      ].some((excluded) => path.includes(excluded));
+    },
+  });
+
+  await fs.move(`../${fileName}`, `./dist/${fileName}`, {
+    overwrite: true,
+  });
+
+  await exec(`cd dist && zip -r ${fileName}.zip ${fileName}`);
+
+  await fs.remove(`./dist/${fileName}`);
+};
+
+const server = (done) => {
+	browserSync.init( {
+		proxy: 'http://mediahub.test/',
+	} );
+	done();
+};
+
+const reload = ( done ) => {
+	browserSync.reload();
+	done();
+};
+
+const cleanDir = () => del(['dist']);
+
+export const watchChanges = () => {
+  watch(['sass/**/*.scss'], series(scss, reload));
+  watch(['js/**/*.js', '**/*.php'], reload);
+};
+
+export const start = series(scss, reload, server, watchChanges);
+export const build = series(scss, cleanDir, compress);
