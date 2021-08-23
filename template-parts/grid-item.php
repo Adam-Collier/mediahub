e67b23d6743d1e634f371819c00a37f64e81a011
Adam Collier <adamcollierart@gmail.com>
2021-08-23 15:39:46 +0100

    feature: add launch date and status to each grid item

--- template-parts/grid-item.php
+++ template-parts/grid-item.php
@@ -10,15 +10,21 @@
  $thumbnailID = get_post_thumbnail_id($campaignID);
  $campaignAlt = get_post_meta($campaignID, '_wp_attachment_image_alt', true);
  $campaignSrcset = wp_get_attachment_image_srcset($thumbnailID, 'large');
+
+ $launchDate = new DateTime(get_lzb_meta( 'launching' ));
+ $now = new DateTime();
+ $launchText = $launchDate > $now ? "Launching" : "Launched";
 ?>
 
 <article class="grid-item">
     <a href="<?php echo the_permalink() ?>">
-        <header class="entry-header">
+        <header class="grid-item-header">
             <?php
             the_title( '<h2 class="grid-item-title heading">', '</h2>' );
             ?>
+            <p class="grid-item-launching"><?php echo $launchText . date(' jS M \a\t g:ia', strtotime(get_lzb_meta( 'launching' ))) ?></p>
         </header><!-- .entry-header -->
+            <p class="grid-item-status <?php echo strtolower(explode(' ', get_lzb_meta( 'status' ))[0])?>"><?php echo get_lzb_meta( 'status' ) ?></p>
         <div class="img-placeholder">
             <img
                 src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" 
